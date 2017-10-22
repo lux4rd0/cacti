b9e56049ee385975ce668df2db1cba2b503ffd76
Dave Schmid <dave@pulpfree.org>
2017-10-21 22:52:51 -0500

    Create sonos-network.sh

---
+++ scripts/audio/sonos/sonos-network.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Sonos - Network Bandwidth Usage
+#
+# sonos-network_bandwidth.sh <hostname>
+#
+# Provide a Sonos speaker URL and Network Interface and it returns RX and TX metrics, usable as a Cacti script
+#
+# Dave Schmid, dave@pulpfree.org
+# version 0.1
+# 2017-10-21
+#
+
+HOSTNAME=$1
+INTERFACE=$2
+DOMAIN="pulpfree.net"
+URL="http://${HOSTNAME}.${DOMAIN}:1400/status/ifconfig"
+
+echo ${URL}
+
+wget -qO- ${URL} | grep -A 6 ${INTERFACE} | grep "RX bytes:" | awk '{print $2,$6;}' | sed -n 1p | sed 's/bytes/rxBytes/' | sed 's/bytes/txBytes/'
