0a6399fa2669d5fe30921454448d7e1906583f59
Dave Schmid <dave@pulpfree.org>
2017-12-28 16:40:42 -0600

    Update sonos-uptime.sh

--- templates/audio/sonos/sonos-uptime.sh
+++ templates/audio/sonos/sonos-uptime.sh
@@ -6,8 +6,6 @@
 #
 # Provide a Sonos speaker URL and it returns the number of seconds of uptime, usable as a Cacti script
 #
-# Requires html2text Python library - https://pypi.python.org/pypi/html2text
-#
 # Dave Schmid, dave@pulpfree.org
 # version 0.1
 # 2017-12-28
@@ -15,20 +13,6 @@
 
 HOSTNAME=$1
 DOMAIN="pulpfree.net"
-URL="http://${HOSTNAME}.${DOMAIN}:1400/status/uptime"
+URL="http://${HOSTNAME}.${DOMAIN}:1400/status/zp"
 
-days=`html2text --escape-all ${URL} | head -1 | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* users.*//; s/min/minutes/; s/([[:digit:]]+):0?([[:digit:]]+)/\1 hours, \2 minutes/' | grep -Po "[[:digit:]]+ *(?=day)"`
-
-hours=`html2text --escape-all ${URL} | head -1 | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* users.*//; s/min/minutes/; s/([[:digit:]]+):0?([[:digit:]]+)/\1 hours, \2 minutes/' | grep -Po "[[:digit:]]+ *(?=hour)"`
-
-minutes=`html2text --escape-all ${URL} | head -1 | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* users.*//; s/min/minutes/; s/([[:digit:]]+):0?([[:digit:]]+)/\1 hours, \2 minutes/' | grep -Po "[[:digit:]]+ *(?=minute)"`
-
-uptimesecs=$(($minutes*60))
-uptimesecs=$(($hours*3600+$uptimesecs))
-
-
-if [[ ${days} =~ [0-9] ]]; then
-    uptimesecs=$(($days*86400+$uptimesecs))
-fi
-
-echo "${uptimesecs}"
+wget -qO- ${URL} |grep UptimeSeconds |grep -Eo '[0-9]{1,7}' |tail -1
