4a1455d781e3030bf118b107d45deb36f66a2ba0
Dave Schmid <dave@pulpfree.org>
2018-07-23 19:48:18 -0500

    Create ST_temperature_fetch.sh

---
+++ templates/smartthings/ST_temperature_fetch.sh
@@ -0,0 +1,17 @@
+#
+# SmartThings Google Docs - Temperature
+#
+
+LOG="/mnt/attic/logs/metrics"
+
+fetch=`wget -qO - https://docs.google.com/spreadsheets/d/<replace>/export?exportFormat=csv | dos2unix | sed '1!{$!d}' |sed -r 's/ temperature//g' |sed -r 's/,,/,null,/g' |sed -r 's/,,/,null,/g'|sed -r 's/ /_/g' |sed -r 's/,$/,null/g' > ${LOG}/ST_temperature_temp.txt`
+
+for i in {1..40}; do
+
+	sensor=`csvtool col $i ${LOG}/ST_temperature_temp.txt | head -1`
+	value=`csvtool col $i ${LOG}/ST_temperature_temp.txt | tail -1`
+
+		if [[ ${value} =~ [0-9] ]]; then
+		echo ${value} > "${LOG}/ST_temperature_${sensor}.log";
+		fi
+done
