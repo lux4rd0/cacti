441a6de1d3abf292c311ae8045885417d46263bb
Dave Schmid <dave@pulpfree.org>
2018-07-23 20:21:51 -0500

    Create ST_battery_fetch.sh

---
+++ templates/smartthings/ST_battery_fetch.sh
@@ -0,0 +1,17 @@
+#
+# SmartThings Google Docs - Battery
+#
+
+LOG="/mnt/attic/logs/metrics"
+
+fetch=`wget -qO - https://docs.google.com/spreadsheets/d/<replace>/export?exportFormat=csv | dos2unix | sed '1!{$!d}' |sed -r 's/ battery//g' |sed -r 's/,,/,null,/g' |sed -r 's/,,/,null,/g'|sed -r 's/ /_/g' |sed -r 's/,$/,null/g' > ${LOG}/ST_battery_temp.txt`
+
+for i in {1..40}; do
+
+	device=`csvtool col $i ${LOG}/ST_battery_temp.txt | head -1`
+	value=`csvtool col $i ${LOG}/ST_battery_temp.txt | tail -1`
+
+		if [[ ${value} =~ [0-9] ]]; then
+		echo ${value} > "${LOG}/ST_battery_${device}.log";
+		fi
+done
