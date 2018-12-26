004ec1ac0156c2b39969de6987babb55bbae3a25
Dave Schmid <dave@pulpfree.org>
2018-12-26 16:47:29 -0600

    Update network-internetbandwidth.sh

--- templates/network/darkstat/network-internetbandwidth.sh
+++ templates/network/darkstat/network-internetbandwidth.sh
@@ -6,8 +6,11 @@ HOSTNAME=$1
 
 IPADDRESS=`nslookup ${HOSTNAME} | awk '/^Address: / { print $2 }'`
 
-rx=`wget --quiet -O - "http://10.10.0.1:666/hosts/?full=yes" |grep -A 3 -i "${IPADDRESS}/" |tail -n 1 |tail -n 1 |sed 's/ <td class="num">//'|sed 's/<\/td>//' |sed 's/,//g'`
+if [[ ${IPADDRESS} =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
 
+rx=`wget --quiet -O - "http://10.10.0.1:666/hosts/?full=yes" |grep -A 3 -i "${IPADDRESS}/" |tail -n 1 |tail -n 1 |sed 's/ <td class="num">//'|sed 's/<\/td>//' |sed 's/,//g'`
 tx=`wget --quiet -O - "http://10.10.0.1:666/hosts/?full=yes" |grep -A 4 -i "${IPADDRESS}/" |tail -n 1 |tail -n 1 |sed 's/ <td class="num">//'|sed 's/<\/td>//' |sed 's/,//g'`
+
+fi
 
 echo "rx:${rx} tx:${tx}"
