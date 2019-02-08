53557fb077a8c084d5faba216dc8b1a25ce6c5d0
Dave Schmid <dave@pulpfree.org>
2019-02-07 18:53:28 -0600

    Delete tesla-fetch_metrics.sh

--- templates/vehicles/tesla/tesla-fetch_metrics.sh
+++
@@ -1,3 +0,0 @@
-teslacmd -u $1 -p $2 -cdtv > /mnt/attic/logs/metrics/tesla_metrics.txt.tmp
-
-cat /mnt/attic/logs/metrics/tesla_metrics.txt.tmp | sed 's/,/ /g' | tr -d '"' | tr -d '{' | tr -d '}' | tr '\n' ' ' |sed 's/: /:/g' |sed 's/    / /g' | tr -s ' ' | sed 's/'\''//g' | cut -c 2- > /mnt/attic/logs/metrics/tesla_metrics.txt
